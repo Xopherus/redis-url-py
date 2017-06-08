9d9bf6ad17aad273a9a923c2e9c51fe4581414d0
Xopherus <craborg@zerofox.com>
2017-06-08 13:23:54 -0400

    Fix py_modules. Add license to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='redis-url-py',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/Xopherus/redis-url-py',
-    license='',
+    license='BSD',
     author='Chris Raborg',
     author_email='craborg1@umbc.edu',
     description='Use Redis URLs in your Python applications',
-    py_modules=['redis_url_py'],
+    py_modules=['redis_url'],
     zip_safe=False,
 )
