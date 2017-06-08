d0122c0862e0024021fcf24b8c4a0c901287d5d5
Xopherus <craborg@zerofox.com>
2017-06-08 13:24:42 -0400

    Bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='redis-url-py',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/Xopherus/redis-url-py',
     license='BSD',
     author='Chris Raborg',
